3350b78a5b42e9eac56881e4fe13f83bac33618c
LoncaricLaura <laura.loncaric032@gmail.com>
2025-06-21 23:55:04 +0200

    added ERC-20 token contract, added functions for rewards in Profile contract

--- contracts/Profile.sol
+++ contracts/Profile.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 contract Profile {
     struct User {
         string name;
@@ -10,18 +12,28 @@ contract Profile {
     }
 
     mapping (address => User) private users;
-    mapping (address => bool) private owners;
+
+    mapping(address => bool) public hasPosted;
+    mapping(address => bool) public hasClaimedFirstPostReward;
+
+    mapping(bytes32 => bool) public postRewarded;
+
+    mapping(bytes32 => address) public postAuthor;
+
+    IERC20 public sparkToken;
+    uint256 public firstPostReward = 10 * 1e18;
+    uint256 public likeMilestoneReward = 5 * 1e18;
 
     event UserRegistered(address userAddress, string name, string description, string profileImageCid);
     event UserUpdated(address userAddress, string name, string description, string profileImageCid);
-    
-    constructor() {
-        owners[msg.sender] = true;
-    }
-    
-    modifier onlyOwner() {
-        require(owners[msg.sender] == true, "Only owner can call this function");
-        _;
+    event FirstPostMarked(address indexed user);
+    event FirstPostRewardClaimed(address indexed user);
+    event PostLiked(bytes32 indexed postId, address indexed liker, uint256 totalLikes);
+    event PostRewarded(bytes32 indexed postId, address indexed author);
+    event Tipped(address indexed from, address indexed to, uint256 amount);
+
+    constructor(address _sparkTokenAddress) {
+        sparkToken = IERC20(_sparkTokenAddress);
     }
     
     function registerUser(string memory _name, string memory _description, string memory _profileImageCid) public {
@@ -49,13 +61,34 @@ contract Profile {
     function getUser(address userAddress) public view returns (string memory name, string memory description, string memory profileImageCid) {
         return (users[userAddress].name, users[userAddress].description, users[userAddress].profileImageCid);
     }
-    
-    function promoteOwner(address ownerAddress) public onlyOwner {
-        owners[ownerAddress] = true;
+
+    function markFirstPost() public {
+        require(!hasPosted[msg.sender], "Already marked as posted");
+        hasPosted[msg.sender] = true;
+        emit FirstPostMarked(msg.sender);
     }
-    
-    function demoteOwner(address ownerAddress) public onlyOwner {
-        require(msg.sender != ownerAddress, "Cannot demote self");
-        owners[ownerAddress] = false;
+
+    function claimFirstPostReward() public {
+        require(hasPosted[msg.sender], "User has not made a post");
+        require(!hasClaimedFirstPostReward[msg.sender], "Reward already claimed");
+        require(sparkToken.balanceOf(address(this)) >= firstPostReward, "Insufficient reward balance");
+
+        hasClaimedFirstPostReward[msg.sender] = true;
+        require(sparkToken.transfer(msg.sender, firstPostReward), "Transfer failed");
+        emit FirstPostRewardClaimed(msg.sender);
+    }
+
+    function claimLikeReward(bytes32 postId) public {
+        require(!postRewarded[postId], "Already rewarded");
+        require(sparkToken.balanceOf(address(this)) >= likeMilestoneReward, "Insufficient reward balance");
+
+        postRewarded[postId] = true;
+        require(sparkToken.transfer(msg.sender, likeMilestoneReward), "Transfer failed");
+        emit PostRewarded(postId, msg.sender);
+    }
+
+    function tip(address to, uint256 amount) public {
+        require(sparkToken.transferFrom(msg.sender, to, amount), "Tip transfer failed");
+        emit Tipped(msg.sender, to, amount);
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,22 +1,34 @@
 const { ethers } = require("hardhat");
+const { parseUnits } = require("ethers");
 
 async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  const SparkToken = await ethers.getContractFactory("SparkToken");
+  const initialSupply = parseUnits("1000000", 18); 
+  const sparkToken = await SparkToken.deploy(initialSupply);
+  await sparkToken.waitForDeployment();
+
+  const sparkTokenAddress = await sparkToken.getAddress();
+  console.log("SparkToken deployed to:", sparkTokenAddress);
+
   const Profile = await ethers.getContractFactory("Profile");
-  const profile = await Profile.deploy();
+  const profile = await Profile.deploy(sparkTokenAddress);
   await profile.waitForDeployment();
 
-  console.log("Profile deployed to:", await profile.getAddress());
+  const profileAddress = await profile.getAddress();
+  console.log("Profile deployed to:", profileAddress);
 
-  const Post = await ethers.getContractFactory("Post");
-  const post = await Post.deploy();
-  await post.waitForDeployment();
+  const rewardAmount = parseUnits("10000", 18); 
+  const transferTx = await sparkToken.transfer(profileAddress, rewardAmount);
+  await transferTx.wait();
 
-  console.log("Post deployed to:", await post.getAddress());
+  console.log("Transferred reward tokens to Profile contract");
 }
 
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
+    console.error("Deployment failed:", error);
     process.exit(1);
   });
